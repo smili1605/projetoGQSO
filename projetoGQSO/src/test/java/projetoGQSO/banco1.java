cc71d89ce8d2374d0b160f7c89e386e7b3557bd1
Joao Guilherme <joaoguilhermesilvasouza@gmail.com>
2021-04-26 01:17:44 -0300

    fazendo o deposito e linkando com com class main

--- projetoGQSO/src/test/java/projetoGQSO/banco1.java
+++ projetoGQSO/src/test/java/projetoGQSO/banco1.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
+import projetoGQSO.Banco.SaldoInsuficiente;
 
 public class banco1 {
     Banco banco;
@@ -21,5 +21,11 @@ public class banco1 {
     public void testeSaque() throws SaldoInsuficiente {
         assertEquals(banco.saldo() - 50, banco.saque(50));
         assertEquals(banco.saldo() - 50, banco.saque(50));
+        assertThrows(Banco.SaldoInsuficiente.class, () -> banco.saque(200));
+    }
+
+    @Test
+    public void testeDeposito() {
+        assertEquals(banco.saldo() + 100, banco.deposito(100));
     }
 }
